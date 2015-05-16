45a8b36aff7946d46894175832878bd8c8de2451
pjotrp <pjotr.public01@thebird.nl>
2015-05-16 15:56:32 -0500

    tests

--- scripts/regressiontests.sh
+++ scripts/regressiontests.sh
@@ -5,8 +5,14 @@
 #  ./scripts/test_run.sh &> test.out
 #
 
-pylmm_path=my_pylmm
-pylmm_lib_path=my_pylmm/pyLMM
+pylmm_path=pylmm_gn2
+pylmm_lib_path=pylmm_gn2/pylmm_gn2
+runlmm="env PYTHONPATH=$pylmm_lib_path python $pylmm_path/bin/runlmm.py"
+
+if [ ! -x $runlmm ]; then
+    echo "Can not run $runlmm"
+    exit 1
+fi
 
 if [ ! -d $pylmm_lib_path ]; then
     echo "The symlink to $pylmm_path is not working!"
@@ -14,43 +20,43 @@ if [ ! -d $pylmm_lib_path ]; then
 fi
 
 echo "small_run.new"
-env PYTHONPATH=$pylmm_lib_path:./lib python $pylmm_lib_path/runlmm.py --pheno data/small.pheno --geno data/small.geno run > test/data/regression/small_run.new
+$runlmm --pheno data/small.pheno --geno data/small.geno run > test/data/regression/small_run.new
 [ $? -ne 0 ] && exit 1
 
 echo "small.new"
-env PYTHONPATH=$pylmm_lib_path:./lib python $pylmm_lib_path/runlmm.py --pheno data/small.pheno --geno data/small.geno redis > test/data/regression/small.new
+$runlmm --pheno data/small.pheno --geno data/small.geno redis > test/data/regression/small.new
 [ $? -ne 0 ] && exit 1
 
 echo "small_na.new"
-env PYTHONPATH=$pylmm_lib_path:./lib python $pylmm_lib_path/runlmm.py --pheno data/small_na.pheno --geno data/small_na.geno redis --remove-missing-phenotypes > test/data/regression/small_na.new
+$runlmm --pheno data/small_na.pheno --geno data/small_na.geno redis --remove-missing-phenotypes > test/data/regression/small_na.new
 [ $? -ne 0 ] && exit 1
 
 echo "small2.new"
-env PYTHONPATH=$pylmm_lib_path:./lib python $pylmm_lib_path/runlmm.py --pheno data/small.pheno --geno data/small.geno redis_new > test/data/regression/small2.new
+$runlmm --pheno data/small.pheno --geno data/small.geno redis_new > test/data/regression/small2.new
 [ $? -ne 0 ] && exit 1
 
 echo "small_na2.new"
-env PYTHONPATH=$pylmm_lib_path:./lib python $pylmm_lib_path/runlmm.py --pheno data/small_na.pheno --geno data/small_na.geno redis_new > test/data/regression/small_na2.new
+$runlmm --pheno data/small_na.pheno --geno data/small_na.geno redis_new > test/data/regression/small_na2.new
 [ $? -ne 0 ] && exit 1
 
 echo "k_small.new"
-env PYTHONPATH=$pylmm_lib_path:./lib python $pylmm_lib_path/runlmm.py --geno data/small.geno kinship --test-kinship > test/data/regression/k_small.new
+$runlmm --geno data/small.geno kinship --test-kinship > test/data/regression/k_small.new
 [ $? -ne 0 ] && exit 1
 
 echo "k_small_na.new"
-env PYTHONPATH=$pylmm_lib_path:./lib python $pylmm_lib_path/runlmm.py --geno data/small_na.geno kinship --maf-normalization --test-kinship > test/data/regression/k_small_na.new
+$runlmm --geno data/small_na.geno kinship --maf-normalization --test-kinship > test/data/regression/k_small_na.new
 [ $? -ne 0 ] && exit 1
 
 echo "test8000_run.new"
-env PYTHONPATH=$pylmm_lib_path:./lib python $pylmm_lib_path/runlmm.py --pheno data/test8000.pheno --geno data/test8000.geno run > test/data/regression/test8000_run.new
+$runlmm --pheno data/test8000.pheno --geno data/test8000.geno run > test/data/regression/test8000_run.new
 [ $? -ne 0 ] && exit 1
 
 echo "test8000.new"
-env PYTHONPATH=$pylmm_lib_path:./lib python $pylmm_lib_path/runlmm.py --pheno data/test8000.pheno --geno data/test8000.geno redis --remove-missing-phenotypes > test/data/regression/test8000.new
+$runlmm --pheno data/test8000.pheno --geno data/test8000.geno redis --remove-missing-phenotypes > test/data/regression/test8000.new
 [ $? -ne 0 ] && exit 1
 
 echo "test8000_new.new"
-env PYTHONPATH=$pylmm_lib_path:./lib python $pylmm_lib_path/runlmm.py --pheno data/test8000.pheno --geno data/test8000.geno redis_new > test/data/regression/test8000_new.new
+$runlmm --pheno data/test8000.pheno --geno data/test8000.geno redis_new > test/data/regression/test8000_new.new
 [ $? -ne 0 ] && exit 1
 
 diff test/data/regression/small.new test/data/regression/small.ref|grep -v seconds
